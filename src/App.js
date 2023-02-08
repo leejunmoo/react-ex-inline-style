819925918deed224b4c2d4001071d0585965bea2
leejunmoo <dlquddnr122@naver.com>
2023-02-08 10:03:08 +0900

    inline-style-함수 적용방법

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import pokemon from './images/000401.png';
+/* 소수의 중요한 이미지는 css파일이아닌 이렇게 직접 import 해서 불러놓기도함 ex)로고, 파비콘 등 */
+
 
 function App() {
+  let btnstyle = {
+    backgroundColor: '#4caf50',
+    border: "none",
+    color: "white",
+    padding: "15px 32px",
+    textAlign: "center",
+    textDecoration: "none",
+    display: "inline-block",
+    fontSize: "16px"
+  }
+  let name= '둘리';
+  let address = '쌍문동';
+  let userInfo = {
+    name: '둘리',
+    address: '쌍문동'
+  }
+
+  const 함수 = () => {
+    alert('함수 실행');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* <img src={pokemon} /> */}
+    <div style={{
+      backgroundImage: `url(${pokemon})`,
+      width: '400px',
+      heigth: '400px'
+    }}>images??</div>
+      <p>{userInfo.name}는 {userInfo.address}에 삽니다.</p>
+      <button onClick={함수} style={btnstyle}>버튼: 함수실행</button>
+      <div className='main-bg'>
+        main-bg
+      </div>
+    </>
+  )
 }
 
 export default App;
